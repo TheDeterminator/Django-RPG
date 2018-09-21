902a5ee3978988d1ca34bf0046d49601fe5c9872
Alec <afroenygam@gmail.com>
2018-09-20 23:49:57 -0400

    queries.py and README.md updated

---
+++ queries.py
@@ -0,0 +1,3 @@
+# How many total characters are there?
+    # There are len(Characters.objects.all()) total Characters equal to 302
+    # If we are to count characters and all character subtypes than their are 615 <len(Fighter.objects.all()) + len(Fighter.objects.all()) + len(Mage.objects.all()) + len(Cleric.objects.all()) + len(Thief.objects.all()) + len(Necromancer.objects.all())>
